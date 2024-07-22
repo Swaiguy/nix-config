ac1f4c10ff256af5fe4fc657144ff39358668511
Swaiguy <126988847+Swaiguy@users.noreply.github.com>
2024-07-22 09:35:42 +0200

    Add files via upload

--- home/linux/gui/hyprland/values/wayland-apps.nix
+++ home/linux/gui/hyprland/values/wayland-apps.nix
@@ -60,7 +60,37 @@
       package = pkgs.firefox-wayland; # firefox with wayland support
     };
 
-    #vscode = get from ryan's wayland-apps.nix
+    vscode = {
+      enable = true;
+      # let vscode sync and update its configuration & extensions across devices, using github account.
+      userSettings = {};
+      package =
+        (pkgs.vscode.override
+          {
+            isInsiders = true;
+            # https://wiki.archlinux.org/title/Wayland#Electron
+            commandLineArgs = [
+              "--ozone-platform-hint=auto"
+              "--ozone-platform=wayland"
+              # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+              # (only supported by chromium/chrome at this time, not electron)
+              "--gtk-version=4"
+              # make it use text-input-v1, which works for kwin 5.27 and weston
+              "--enable-wayland-ime"
 
+              # TODO: fix https://github.com/microsoft/vscode/issues/187436
+              # still not works...
+              "--password-store=gnome" # use gnome-keyring as password store
+            ];
+          })
+        .overrideAttrs (oldAttrs: rec {
+          # Use VSCode Insiders to fix crash: https://github.com/NixOS/nixpkgs/issues/246509
+          src = builtins.fetchTarball {
+            url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+            sha256 = "16ij15mdrqi0k0nqcpfl5vvyf623amhvmka58jxa9a7vncji707v";
+          };
+          version = "latest";
+        });
+    };
   };
 }
